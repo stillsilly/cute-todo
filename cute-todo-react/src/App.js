d97fcd4bb85fea9f0f415b74d60b638d736e552b
羊羊 <1690374201@qq.com>
2020-04-12 01:52:54 +0800

    添加sass支持，好折腾

--- cute-todo-react/src/App.js
+++ cute-todo-react/src/App.js
@@ -1,21 +1,55 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import './App.scss';
+import './home.scss';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="page-wrapper page-wrapper-home">
+                <h1 className="page-title">代办事项</h1>
+                <div className="section-main">
+                    <div className="row-input">
+                        <button className="btn-toggle-collapse">收起</button>
+                        <input type="text" placeholder="添加代办事项，回车可保存"/>
+                        <button>确定</button>
+                    </div>
+                    <ol className="todo-list">
+                        <li className="todo-item">
+                            <input type="checkbox"/>
+                            <span>吃饭饭</span>
+                            <button>删除</button>
+                        </li>
+                        <li className="todo-item">
+                            <input type="checkbox"/>
+                            <span>喝水</span>
+                            <button>删除</button>
+                        </li>
+                        <li className="todo-item">
+                            <input type="checkbox"/>
+                            <span>看书</span>
+                            <button>删除</button>
+                        </li>
+                        <li className="todo-item">
+                            <input type="checkbox"/>
+                            <span>运动</span>
+                            <button>删除</button>
+                        </li>
+                    </ol>
+                    <div className="row-bottom">
+                <span className="info-count">
+                    还剩 <span className="num">3</span> 项未完成
+                </span>
+                        <ul className="filter-btn-list">
+                            <button className="btn-filter-all">全部</button>
+                            <button className="btn-filter-active">未完成</button>
+                            <button className="btn-filter-completed">已完成</button>
+                        </ul>
+                        <button className="btn-delete-completed">清空已完成</button>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
