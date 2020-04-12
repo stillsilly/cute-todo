fced43e6c25a77f104ce4755d5d3fa0f0de652af
羊羊 <1690374201@qq.com>
2020-04-13 03:25:05 +0800

    样式

--- cute-todo-react/src/App.js
+++ cute-todo-react/src/App.js
@@ -9,42 +9,43 @@ class App extends Component {
                 <h1 className="page-title">代办事项</h1>
                 <div className="section-main">
                     <div className="row-input">
-                        <button className="btn-toggle-collapse">收起</button>
-                        <input type="text" placeholder="添加代办事项，回车可保存"/>
-                        <button>确定</button>
+                        <input className="input-todo" type="text" placeholder="添加代办事项，回车可保存"/>
+                        <button className="btn-confirm">确定</button>
+                    </div>
+                    <div className="row-operate">
+                        <ul className="filter-btn-list">
+                            <button className="btn-filter btn-filter-all btn-filter_active">全部</button>
+                            <button className="btn-filter btn-filter-active">未完成</button>
+                            <button className="btn-filter btn-filter-completed">已完成</button>
+                        </ul>
+                        <button className="btn-delete-completed">清空已完成</button>
                     </div>
                     <ol className="todo-list">
                         <li className="todo-item">
-                            <input type="checkbox"/>
-                            <span>吃饭饭</span>
-                            <button>删除</button>
+                            <input className="complete-checkbox" type="checkbox"/>
+                            <input className="item-text" type="text" value="吃饭饭"/>
+                            <button className="btn-delete">删除</button>
                         </li>
                         <li className="todo-item">
-                            <input type="checkbox"/>
-                            <span>喝水</span>
-                            <button>删除</button>
+                            <input className="complete-checkbox" type="checkbox"/>
+                            <input className="item-text" type="text" value="喝水"/>
+                            <button className="btn-delete">删除</button>
                         </li>
                         <li className="todo-item">
-                            <input type="checkbox"/>
-                            <span>看书</span>
-                            <button>删除</button>
+                            <input className="complete-checkbox" type="checkbox"/>
+                            <input className="item-text" type="text" value="看书"/>
+                            <button className="btn-delete">删除</button>
                         </li>
                         <li className="todo-item">
-                            <input type="checkbox"/>
-                            <span>运动</span>
-                            <button>删除</button>
+                            <input className="complete-checkbox" type="checkbox"/>
+                            <input className="item-text" type="text" value="运动"/>
+                            <button className="btn-delete">删除</button>
                         </li>
                     </ol>
                     <div className="row-bottom">
-                <span className="info-count">
-                    还剩 <span className="num">3</span> 项未完成
-                </span>
-                        <ul className="filter-btn-list">
-                            <button className="btn-filter-all">全部</button>
-                            <button className="btn-filter-active">未完成</button>
-                            <button className="btn-filter-completed">已完成</button>
-                        </ul>
-                        <button className="btn-delete-completed">清空已完成</button>
+                            <span className="info-count">
+                            还剩 <span className="num">3</span> 项未完成
+                        </span>
                     </div>
                 </div>
             </div>
